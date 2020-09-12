103643db3831c1c3862f5c4bda4344b5e3f65841
Cory Cook <2746394+corycook@users.noreply.github.com>
2020-09-12 23:14:26 +0000

    feat: graphql response lists codenames response

--- src/index.ts
+++ src/index.ts
@@ -27,37 +27,98 @@ interface WordAssociationsSearchResponse {
 
 @type
 class Query {
-  @field(() => [WordAssociationResponse])
-  async stimuli(@arg([String]) text: string[]):
-      Promise<WordAssociationResponse[]> {
-    const wordAssociations = await getWordAssociations(text);
-    return wordAssociations.response.map(r => new WordAssociationResponse(r));
+  @field(() => [Association])
+  async stimuli(@arg([String]) words: string[]): Promise<Association[]> {
+    const associations = await getWordAssociations(words);
+    return removeDuplicates(associations.response.flatMap(
+        r => r.items.map(item => new Association({...item, word: r.text}))));
+  }
+
+  @field(() => [Codeword])
+  async codewords(@arg([String]) words: string[]): Promise<Codeword[]> {
+    const all = await this.stimuli(words);
+    const grouped = groupWordAssociations(all);
+    return Object.keys(grouped)
+        .map(word => new Codeword({word, matches: grouped[word]}))
+        .sort((l, r) => r.weight - l.weight);
   }
 }
 
 @type
-class WordAssociationResponse {
+class Association {
   constructor(private readonly data:
-                  WordAssociationsSearchResponse['response'][0]) {}
+                  WordAssociationsSearchResponse['response'][0]['items'][0]&
+              {word: string}) {}
 
-  @field(String)
-  get text(): string {
-    return this.data.text;
+  @field()
+  get word(): string {
+    return this.data.word;
   }
 
-  @field([String])
-  get associations(): string[] {
-    return this.data.items.map(i => i.item);
+  @field()
+  get relatedWord(): string {
+    return this.data.item;
+  }
+
+  @field()
+  get weight(): number {
+    return this.data.weight;
   }
 }
 
+@type
+class Codeword {
+  @field() private readonly word: string;
+  private readonly matches: Association[];
+
+  constructor(data: {word: string, matches: Association[]}) {
+    this.word = data.word;
+    // remove duplicate related words
+    this.matches = data.matches.reduce(
+        (result: Association[], next) =>
+            result.some(i => i.word == next.word) ? result : [...result, next],
+        []);
+  }
+
+  @field()
+  get count(): number {
+    return this.matches.length;
+  }
+
+  @field()
+  get weight(): number {
+    return this.matches.reduce((sum, match) => sum + match.weight, 0);
+  }
+
+  @field([String])
+  get words(): string[] {
+    return this.matches.map(m => m.word);
+  }
+}
+
+function removeDuplicates<T>(list: T[]): T[] {
+  const db: {[hash: string]: T} = list.reduce(
+      (result, item) => ({...result, [JSON.stringify(item)]: item}), {});
+  return Object.values(db);
+}
 
 async function getWordAssociations(text: string[] = []):
     Promise<WordAssociationsSearchResponse> {
   const textArg = text.map(t => `text=${t}`).join('&');
   const response = await fetch(`${WORDASSOCIATIONS_NET_URL}?apikey=${
-      process.env.WORDASSOCIATIONS_NET_API_KEY}&lang=en&${textArg}`);
+      process.env.WORDASSOCIATIONS_NET_API_KEY}&lang=en&${textArg}&limit=100`);
   return response.json();
+}
+
+function groupWordAssociations(associations: Association[]):
+    {[word: string]: Association[]} {
+  return associations.reduce(
+      (accumulator, current) => ({
+        ...accumulator,
+        [current.relatedWord]:
+            [...(accumulator[current.relatedWord] || []), current],
+      }),
+      {} as {[word: string]: Association[]});
 }
 
 /**
